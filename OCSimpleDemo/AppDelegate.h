74269367d88b67fe6470703ed1faf39a57c581a5
linxiaobin <linxiaobin@felink.com>
2018-08-16 12:43:55 +0800

    Initial Commit

---
+++ OCSimpleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCSimpleDemo
+//
+//  Created by LinXiaoBin on 2018/8/16.
+//  Copyright © 2018年 Felink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCSimpleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCSimpleDemo
+//
+//  Created by LinXiaoBin on 2018/8/16.
+//  Copyright © 2018年 Felink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
